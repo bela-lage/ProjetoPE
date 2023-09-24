1feddf0ad441bad58dbe1bcfaf4fcd3384103abd
bela-lage <isabelalroch@gmail.com>
2023-09-24 13:31:36 -0300

    update on app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 // Aqui ficam as informações principais contidas na minha API
 
 import express from "express";
-
 const app = express();
+
+import cors from "cors";
+app.use(cors());
+
+app.use(express.json());
 
 import db from "./config/database.js"
 db.connect();
